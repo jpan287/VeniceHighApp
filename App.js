9705651c981d46a338421469695667f0475832a9
Johnny Pan <24360840+penlu01@users.noreply.github.com>
2022-07-23 23:34:05 -0700

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { EventsScreen } from './Screens/EventsScreen';
+import { BellScreen } from './Screens/BellScreen';
+import { SearchScreen } from './Screens/SearchScreen';
+import { InfoScreen } from './Screens/InfoScreen';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Events') {
+            iconName = 'ios-list';
+          }
+          else if (route.name === 'Bell') {
+            iconName = 'ios-notifications';
+          }
+          else if (route.name === 'Search') {
+            iconName = 'ios-search';
+          }
+          else if (route.name === 'Info') {
+            iconName = 'ios-information-circle-outline'
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        headerTitle: () => {
+          let tabTitle;
+          if (route.name === 'Events') {
+            tabTitle = 'Upcoming Events';
+          }
+          else if (route.name === 'Bell') {
+            tabTitle = 'Bell Schedules';
+          }
+          else if (route.name === 'Search') {
+            tabTitle = 'Staff List';
+          }
+          else if (route.name === 'Info') {
+            tabTitle = 'Info and Settings';
+          }
+          return tabTitle;
+        },
+        tabBarActiveTintColor: 'dark-gray',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen name="Events" component={EventsScreen} />
+      <Tab.Screen name="Bell" component={BellScreen} />
+      <Tab.Screen name="Search" component={SearchScreen} />
+      <Tab.Screen name="Info" component={InfoScreen} />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
